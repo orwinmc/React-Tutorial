7cafbea268a9fcdda523dae8109ffd082c074f99
Michael Orwin <michaelxorwin@gmail.com>
2019-10-09 13:20:09 -0400

    (2) Bootstrap Working

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Badge from 'react-bootstrap/Badge';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Example heading <Badge variant="secondary">New</Badge>
+      </h1>
+      <h2>
+        Example heading <Badge variant="secondary">New</Badge>
+      </h2>
+      <h3>
+        Example heading <Badge variant="secondary">New</Badge>
+      </h3>
+      <h4>
+        Example heading <Badge variant="secondary">New</Badge>
+      </h4>
+      <h5>
+        Example heading <Badge variant="secondary">New</Badge>
+      </h5>
+      <h6>
+        Example heading <Badge variant="secondary">New</Badge>
+      </h6>
+    </div>
     </div>
   );
 }
