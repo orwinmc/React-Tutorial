0325e810ccb61b22e30d465f54f2d10598383599
Michael Orwin <michaelxorwin@gmail.com>
2019-10-09 15:14:33 -0400

    (5) Using State

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -4,43 +4,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ImportantTable from './components/ImportantTable/ImportantTable.js';
-
-const userInfo = [
-  {
-    firstname: 'Michael',
-    lastname: 'Orwin',
-    username: 'orwinmc'
-  },
-  {
-    firstname: 'Josh',
-    lastname: 'Gibson',
-    username: 'gibsonjosh55'
-  },
-  {
-    firstname: 'Sharif',
-    lastname: 'Shaker',
-    username: 'sharifmsha'
-  },
-  {
-    firstname: 'Erin',
-    lastname: 'Radermacher',
-    username: 'erinr_8'
-  },
-  {
-    firstname: 'Keegan',
-    lastname: 'Bach',
-    username: 'kbeazy'
-  },
-  {
-    firstname: 'Connor',
-    lastname: 'Bach',
-    username: 'cbach120'
-  },
-]
-
 function App() {
     return (
-    <ImportantTable userInfo={userInfo}/>
+    <ImportantTable/>
   );
 }
 
--- tutorial/src/components/ImportantTable/ImportantTable.js
+++ tutorial/src/components/ImportantTable/ImportantTable.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 const TableHeader = () => {
   return (
@@ -9,35 +10,87 @@ const TableHeader = () => {
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
+        <th>Edit</th>
       </tr>
     </thead>
   );
 }
 
 const TableBody = (props) => {
-  var rows = [];
-  for (var i = 0; i < props.userInfo.length; i++) {
-      rows.push(
-        <tr>
-          <td>{i}</td>
-          <td>{props.userInfo[i].firstname}</td>
-          <td>{props.userInfo[i].lastname}</td>
-          <td>@{props.userInfo[i].username}</td>
-        </tr>
-      );
-  }
-  
+  const rows = props.userInfo.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{index}</td>
+        <td>{props.userInfo[index].firstname}</td>
+        <td>{props.userInfo[index].lastname}</td>
+        <td>@{props.userInfo[index].username}</td>
+        <td><Button onClick={() => props.removeUser(index)}>Remove</Button></td>
+      </tr>
+    )
+  });
+
   return <tbody>{rows}</tbody>;
 }
 
 
-const ImportantTable = (props) => {
-  return (
-    <Table bordered hover variant="dark">
-      <TableHeader />
-      <TableBody userInfo={props.userInfo}/>
-    </Table>
-  );
+class ImportantTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { userInfo:
+      [
+        {
+          firstname: 'Michael',
+          lastname: 'Orwin',
+          username: 'orwinmc'
+        },
+        {
+          firstname: 'Josh',
+          lastname: 'Gibson',
+          username: 'gibsonjosh55'
+        },
+        {
+          firstname: 'Sharif',
+          lastname: 'Shaker',
+          username: 'sharifmsha'
+        },
+        {
+          firstname: 'Erin',
+          lastname: 'Radermacher',
+          username: 'erinr_8'
+        },
+        {
+          firstname: 'Keegan',
+          lastname: 'Bach',
+          username: 'kbeazy'
+        },
+        {
+          firstname: 'Connor',
+          lastname: 'Bach',
+          username: 'cbach120'
+        }
+      ]
+    }
+
+    this.removeUser = this.removeUser.bind(this);
+  }
+
+  removeUser(index) {
+    this.setState((state) => ({
+      userInfo: state.userInfo.filter((user, i) => {
+        console.log(i);
+        return i !== index;
+      })
+    }));
+  }
+
+  render() {
+    return (
+      <Table bordered hover>
+        <TableHeader />
+        <TableBody userInfo={this.state.userInfo} removeUser={this.removeUser}/>
+      </Table>
+    )
+  }
 }
 
 export default ImportantTable;
