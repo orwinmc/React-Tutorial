4777934a195dfb0819009e2f8c77b8d9a7d4e3a7
Michael Orwin <michaelxorwin@gmail.com>
2019-10-09 13:39:46 -0400

    (3) First Component

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -3,32 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Badge from 'react-bootstrap/Badge';
+import ImportantTable from './components/ImportantTable/ImportantTable.js';
 
 function App() {
   return (
-    <div className="App">
-    <div>
-      <h1>
-        Example heading <Badge variant="secondary">New</Badge>
-      </h1>
-      <h2>
-        Example heading <Badge variant="secondary">New</Badge>
-      </h2>
-      <h3>
-        Example heading <Badge variant="secondary">New</Badge>
-      </h3>
-      <h4>
-        Example heading <Badge variant="secondary">New</Badge>
-      </h4>
-      <h5>
-        Example heading <Badge variant="secondary">New</Badge>
-      </h5>
-      <h6>
-        Example heading <Badge variant="secondary">New</Badge>
-      </h6>
-    </div>
-    </div>
+    <ImportantTable />
   );
 }
 
